de401ef330be8257cec715312066ab7161928f64
Eric van Wijk <eric@van-wijk.com>
2020-02-18 13:25:37 +0100

    Reconfigure agent

--- scripts/install_agent.sh
+++ scripts/install_agent.sh
@@ -55,41 +55,47 @@ done
 validate
 
 # Allways re-install agent, if it exists
-#if [ -f /etc/systemd/system/vsts.agent.${ORG}.* ]; then
-#    echo "Agent ${AGENT_NAME} already installed, skipping setup"
-#    pushd $HOME/pipeline-agent
-#else
-    # Get latest released version from GitHub
-    # https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest
-    AGENT_VERSION=$(curl https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | jq ".name" | sed -E 's/.*"v([^"]+)".*/\1/')
-    AGENT_PACKAGE="vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz"
-    AGENT_URL="https://vstsagentpackage.azureedge.net/agent/${AGENT_VERSION}/${AGENT_PACKAGE}"
-
-    if [ ! -d $HOME/pipeline-agent ]; then
-        mkdir $HOME/pipeline-agent
-    fi
+if [ -f $HOME/pipeline-agent/.agent ]; then
+    echo "Agent ${AGENT_NAME} already installed, removing first..."
     pushd $HOME/pipeline-agent
-    echo "Retrieving agent from ${AGENT_URL}..."
-    wget $AGENT_URL
-    echo "Extracting ${AGENT_PACKAGE} in $(pwd)..."
-    tar zxf $AGENT_PACKAGE
-    echo "Extracted ${AGENT_PACKAGE}"
+    sudo ./svc.sh stop
+    sudo ./svc.sh uninstall
+    ./config.sh remove \
+              --unattended \
+              --auth pat --token $PAT
+fi
 
-    # Unattended config
-    # https://docs.microsoft.com/en-us/azure/devops/pipelines/agents/v2-linux?view=azure-devops#unattended-config
-    echo "Creating agent ${AGENT_NAME} and adding it to pool ${AGENT_POOL} in organization ${ORG}..."
-    ./config.sh --unattended \
-                --url https://dev.azure.com/${ORG} \
-                --auth pat \
-                --token $PAT \
-                --pool $AGENT_POOL \
-                --agent $AGENT_NAME --replace \
-                --acceptTeeEula
+# Get latest released version from GitHub
+# https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest
+AGENT_VERSION=$(curl https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | jq ".name" | sed -E 's/.*"v([^"]+)".*/\1/')
+AGENT_PACKAGE="vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz"
+AGENT_URL="https://vstsagentpackage.azureedge.net/agent/${AGENT_VERSION}/${AGENT_PACKAGE}"
 
+if [ ! -d $HOME/pipeline-agent ]; then
+    mkdir $HOME/pipeline-agent
+fi
+pushd $HOME/pipeline-agent
+echo "Retrieving agent from ${AGENT_URL}..."
+wget $AGENT_URL
+echo "Extracting ${AGENT_PACKAGE} in $(pwd)..."
+tar zxf $AGENT_PACKAGE
+echo "Extracted ${AGENT_PACKAGE}"
+
+# Unattended config
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/agents/v2-linux?view=azure-devops#unattended-config
+echo "Creating agent ${AGENT_NAME} and adding it to pool ${AGENT_POOL} in organization ${ORG}..."
+./config.sh --unattended \
+            --url https://dev.azure.com/${ORG} \
+            --auth pat --token $PAT \
+            --pool $AGENT_POOL \
+            --agent $AGENT_NAME --replace \
+            --acceptTeeEula
+
+if [ ! -f /etc/systemd/system/vsts.agent.${ORG}.* ]; then
     # Run as systemd service
     echo "Setting up agent to run as systemd service..."
     sudo ./svc.sh install
-#fi
+fi
 
 echo "Starting agent service..."
 sudo ./svc.sh start

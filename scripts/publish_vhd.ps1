e87b5b3f9eb946fd584f84aa08b706f7d6789e6a
Eric van Wijk <eric@van-wijk.com>
2022-01-22 11:57:11 +0100

    Test publish

---
+++ scripts/publish_vhd.ps1
@@ -0,0 +1,85 @@
+#!/usr/bin/env pwsh
+
+<# 
+.SYNOPSIS 
+    Finds VHD in given Resource Group and publishes it in Shared Image Gallery
+ 
+.DESCRIPTION 
+    The image build script (https://github.com/actions/virtual-environments/blob/main/helpers/GenerateResourcesAndImage.ps1)
+    creates a storage account, storage container and VHD. This scipt finfs the VHD based on the Resource Group name that is input to aforementioned script.
+
+.EXAMPLE
+    ./publish_vhd.ps1 packer-99999 -GalleryResourceGroupName Shared -GalleryName testgal -ImageDefinitionName UbuntuPipelineHost -Publisher PrivatePipelineImages -Offer Ubuntu -SKU 18 -OsType linux
+#> 
+#Requires -Version 7
+
+### Arguments
+param ( 
+    [parameter(Mandatory=$true)][string]$PackerResourceGroupName,
+    [parameter(Mandatory=$false)][string]$GalleryResourceGroupName,
+    [parameter(Mandatory=$false)][string]$GalleryName,
+    [parameter(Mandatory=$false)][string]$ImageDefinitionName,
+    [parameter(Mandatory=$false)][string]$ImageDefinitionVersion,
+    [parameter(Mandatory=$false,HelpMessage="Only required if Gallery Image Definition does not exist yet")][string]$Publisher,
+    [parameter(Mandatory=$false,HelpMessage="Only required if Gallery Image Definition does not exist yet")][string]$Offer,
+    [parameter(Mandatory=$false,HelpMessage="Only required if Gallery Image Definition does not exist yet")][string]$SKU,
+    [parameter(Mandatory=$false,HelpMessage="Only required if Gallery Image Definition does not exist yet")][string]$OsType
+) 
+
+# Find VHD in Packer Resource Group
+az storage account list -g $PackerResourceGroupName --query "[0]" -o json | ConvertFrom-Json | Set-Variable storageAccount
+$storageAccountKey =  $(az storage account keys list -n $storageAccount.name --query "[0].value" -o tsv)
+$vhdPath = $(az storage blob directory list -c system -d "Microsoft.Compute/Images/images" --account-name $storageAccount.name --account-key $storageAccountKey --query "[?ends_with(name,'vhd')].name" -o tsv)
+$vhdUrl = "$($storageAccount.primaryEndpoints.blob)$vhdPath"
+Write-Host "`nVHD: $vhdUrl"
+
+# Image Gallery
+if (!$GalleryResourceGroupName -or !$GalleryName) {
+    Write-Warning "Shared Image Gallery not specified, exiting"
+    exit
+}
+
+az group list --query "[?name=='$GalleryResourceGroupName']" -o json | ConvertFrom-Json | Set-Variable galleryResourceGroup
+$tags=@("application=Pipeline Agents","provisioner=azure-cli")
+if (!$galleryResourceGroup) {
+    Write-Host "Shared Gallery Resource Group '$GalleryResourceGroupName' does not exist yet, creating it..."
+    az group create -n $GalleryResourceGroupName -l $storageAccount.primaryLocation --tags $tags -o json | ConvertFrom-Json | Set-Variable galleryResourceGroup
+}
+az sig list --resource-group $GalleryResourceGroupName --query "[?name=='$GalleryName']" -o json | ConvertFrom-Json | Set-Variable gallery
+if (!$gallery) {
+    Write-Host "Shared Gallery '$GalleryName' does not exist yet, creating it..."
+
+    # az feature register --namespace Microsoft.Compute --name SIGSoftDelete
+    # az sig create --resource-group $GalleryResourceGroupName --gallery-name $GalleryName --soft-delete -l $galleryResourceGroup.location --tags $tags
+    az sig create --gallery-name $GalleryName --resource-group $GalleryResourceGroupName -l $galleryResourceGroup.location --tags $tags
+}
+
+if (!$ImageDefinitionName) {
+    Write-Warning "Image Definition not specified, exiting"
+    exit
+}
+az sig image-definition list --gallery-name $GalleryName --resource-group $GalleryResourceGroupName --query "[?name=='$ImageDefinitionName']" -o json | ConvertFrom-Json | Set-Variable imageDefinition
+if (!$imageDefinition) {
+    if (!$Publisher -or !$Offer -or !$SKU -or !$OsType) {
+        Write-Warning "Image Definition '$ImageDefinitionName' does not exist yet and arguments to create it were not (all) not specified, exiting"
+        exit
+    }
+    Write-Host "Image Definition '$imageDefinition' does not exist yet, creating it..."
+    az sig image-definition create --gallery-image-definition $ImageDefinitionName `
+                                   --gallery-name $GalleryName `
+                                   --resource-group $GalleryResourceGroupName `
+                                   --publisher $Publisher --offer $Offer --sku $SKU `
+                                   --os-type $OsType --os-state Generalized `
+                                   --tags $tags | ConvertFrom-Json | Set-Variable imageDefinition
+
+    # $version = $(git log -1 --format=%cs <packerfile>.json) -replace "-",""
+
+    az sig image-version create --gallery-image-definition $ImageDefinitionName `
+                                --gallery-name $GalleryName `
+                                --gallery-image-version $ImageDefinitionVersion `
+                                --resource-group $GalleryResourceGroupName `
+                                --os-vhd-uri $vhdUrl `
+                                --tags $tags | ConvertFrom-Json | Set-Variable imageVersion
+    $imageVersion | Format-List
+}
+

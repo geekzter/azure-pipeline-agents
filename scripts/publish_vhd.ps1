100ef15ad79542bab1e480708dda3f331d720c70
Eric van Wijk <eric@van-wijk.com>
2022-01-23 09:19:50 +0100

    Fix JMESPath

--- scripts/publish_vhd.ps1
+++ scripts/publish_vhd.ps1
@@ -36,9 +36,10 @@ if (!$packerResourceGroup) {
 
 # Find VHD in Packer Resource Group
 az storage account list -g $PackerResourceGroupName --query "[0]" -o json | ConvertFrom-Json | Set-Variable storageAccount
-$storageAccountKey =  $(az storage account keys list -n $storageAccount.name --query "[0].value" -o tsv)
+$storageAccountKey =  $(az storage account keys list -n $($storageAccount.name) --query "[0].value" -o tsv)
 # TODO: Use SAS or RBAC
-$vhdPath = $(az storage blob directory list -c system -d "Microsoft.Compute/Images/images" --account-name $storageAccount.name --account-key "${storageAccountKey}" --query "[?ends_with(name,'vhd')].name" -o tsv)
+Write-Debug "az storage blob directory list -c system -d `"Microsoft.Compute/Images/images`" --account-name $($storageAccount.name) --account-key `"${storageAccountKey}`" --query `"[?ends_with(@.name, 'vhd')].name`" -o tsv"
+$vhdPath = $(az storage blob directory list -c system -d "Microsoft.Compute/Images/images" --account-name $($storageAccount.name) --account-key "${storageAccountKey}" --query "[?ends_with(@.name, 'vhd')].name" -o tsv)
 $vhdUrl = "$($storageAccount.primaryEndpoints.blob)system/${vhdPath}"
 Write-Host "`nVHD: $vhdUrl"
 
@@ -104,7 +105,7 @@ if ($imageVersion) {
                                 --gallery-image-version $newVersionString `
                                 --resource-group $GalleryResourceGroupName `
                                 --os-vhd-uri $vhdUrl `
-                                --os-vhd-storage-account $storageAccount.name `
+                                --os-vhd-storage-account $($storageAccount.name) `
                                 --tags $imageTags | ConvertFrom-Json | Set-Variable imageVersion
 }
 

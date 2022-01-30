a9b995ca0352b1c353125b6c4a83ed05f80b98c8
Eric van Wijk <eric@van-wijk.com>
2022-01-30 10:07:35 +0100

    checkout tag

---
+++ scripts/list_image_releases.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/env pwsh
+<#
+.SYNOPSIS 
+    Lists image releases
+ 
+.DESCRIPTION 
+    Downloads latest image releases from https://github.com/repos/actions/virtual-environments
+#> 
+
+(Invoke-RestMethod -Uri "https://api.github.com/repos/actions/virtual-environments/releases") | ForEach-Object {
+    $_ | Add-Member -NotePropertyName Image   -NotePropertyValue $_.tag_name.Split("/")[0]
+    $_ | Add-Member -NotePropertyName Preview -NotePropertyValue (($_.draft -ieq "true") -or ($_.prerelease -ieq "true"))
+    $_ | Add-Member -NotePropertyName Version -NotePropertyValue $_.tag_name.Split("/")[1]
+    $_ | Add-Member -NotePropertyName Commit -NotePropertyValue $_.target_commitish
+    $_
+} | Select-Object -Property Name, id, Image, Version, Preview, Commit | Format-Table

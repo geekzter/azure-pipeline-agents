075d805f2e52cf06ec435ea19a36026033c91c9e
Eric van Wijk <eric@van-wijk.com>
2022-02-07 08:38:30 +0100

    Policy prep

---
+++ terraform/modules/packer/policy.tf
@@ -0,0 +1,9 @@
+resource azurerm_policy_definition no_vm_extension {
+  name                         = "no-vm-extension-policy"
+  policy_type                  = "Custom"
+  mode                         = "Indexed"
+  description                  = "VM extensions that are installed during VM image build time lead to non-deterministic outcomes, this policy aims to prevent that"
+  display_name                 = "Prevent VM extensions on Packer Build VM's"
+
+  policy_rule                  = file("${path.module}/no-vm-extension-policy.json")
+}

d1fcf9daa9bb4f5a5012d8ebed06c542cd474948
Eric van Wijk <eric@van-wijk.com>
2022-02-10 11:46:56 +0100

    full-uri

--- scripts/get_vhd.ps1
+++ scripts/get_vhd.ps1
@@ -66,17 +66,26 @@ $vhdUrl = "$($storageAccount.primaryEndpoints.blob)${storageContainerName}/${vhd
 $vhdUrlResult = $vhdUrl
 if ($GenerateSAS) {
     # $sasToken=$(az storage blob generate-sas -c $storageContainerName -n $vhdPath --account-name $storageAccountName --permissions r --expiry (Get-Date).AddDays(7).ToString("yyyy-MM-dd") --subscription $packerSubscriptionId -o tsv)
-    az storage account generate-sas --account-key $(az storage account keys list -n $storageAccountName -g $packerResourceGroupName --subscription $packerSubscriptionId --query "[0].value" -o tsv) `
-                                    --account-name $storageContainerName `
-                                    --expiry "$([DateTime]::UtcNow.AddDays(7).ToString('s'))Z" `
-                                    --permissions "lracuw"`
-                                    --resource-types co `
-                                    --services b `
-                                    --subscription $packerSubscriptionId `
-                                    --start "$([DateTime]::UtcNow.AddDays(-30).ToString('s'))Z" `
-                                    -o tsv | Set-Variable sasToken    
+    az storage blob generate-sas -c $storageContainerName `
+                                 -n $vhdPath `
+                                 --account-name $storageAccountName `
+                                 --permissions acdrw `
+                                 --expiry (Get-Date).AddDays(7).ToString("yyyy-MM-dd") `
+                                 --start "$([DateTime]::UtcNow.AddDays(-30).ToString('s'))Z" `
+                                 --subscription $packerSubscriptionId `
+                                 --full-uri `
+                                 -o tsv | Set-Variable vhdUrlResult
+    # az storage account generate-sas --account-key $(az storage account keys list -n $storageAccountName -g $packerResourceGroupName --subscription $packerSubscriptionId --query "[0].value" -o tsv) `
+    #                                 --account-name $storageContainerName `
+    #                                 --expiry "$([DateTime]::UtcNow.AddDays(7).ToString('s'))Z" `
+    #                                 --permissions "lracuw"`
+    #                                 --resource-types co `
+    #                                 --services b `
+    #                                 --subscription $packerSubscriptionId `
+    #                                 --start "$([DateTime]::UtcNow.AddDays(-30).ToString('s'))Z" `
+    #                                 -o tsv | Set-Variable sasToken    
 
-    $vhdUrlResult = "${vhdUrl}?${sasToken}"
+    # $vhdUrlResult = "${vhdUrl}?${sasToken}"
 }
 
 Write-Host "`nVHD: $vhdUrlResult"

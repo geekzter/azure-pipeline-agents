c9bbfcb7456a9439ccfda1d1bb27618ea1e68ecb
Eric van Wijk <eric@van-wijk.com>
2022-05-11 09:15:16 +0200

    Disable file share by default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,7 +34,7 @@ variable deploy_bastion {
 }
 variable deploy_files_share {
   description                  = "Deploys files share (e.g. for agent diagnostics)"
-  default                      = true
+  default                      = false
   type                         = bool
 }
 variable deploy_firewall {

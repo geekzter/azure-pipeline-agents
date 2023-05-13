16fe98b3cfc7179cbdd8ea3877cc5042f8be4620
Eric van Wijk <eric@van-wijk.com>
2023-05-13 12:15:27 +0200

    Use OIDC

--- scripts/set_terraform_azurerm_vars.ps1
+++ scripts/set_terraform_azurerm_vars.ps1
@@ -1,24 +1,83 @@
 #!/usr/bin/env pwsh
 
-$pipeline = ![string]::IsNullOrEmpty($env:AGENT_VERSION)
-if ($pipeline) {
-    # Propagate pipeline Service Principal as Terraform variables
-    $env:ARM_CLIENT_ID       ??= $env:servicePrincipalId
-    $env:ARM_CLIENT_SECRET   ??= $env:servicePrincipalKey
-    $env:ARM_TENANT_ID       ??= $env:tenantId
-} else {
-    Write-Warning "Not in a pipeline, no context to propagate as ARM_TENANT_ID & ARM_SUBSCRIPTION_ID environment variables"
+function Get-OidcRequestToken()
+{
+    if (!$env:SYSTEM_ACCESSTOKEN) {
+        throw "SYSTEM_ACCESSTOKEN not found"
+    }
+    return $env:SYSTEM_ACCESSTOKEN
 }
 
-if (Get-Command az -ErrorAction SilentlyContinue) {
-    # Get from Azure CLI context
-    az account show 2>$null | ConvertFrom-Json | Set-Variable account
-    if ($account) {
-        $env:ARM_TENANT_ID       ??= $account.tenantId
-        $env:ARM_SUBSCRIPTION_ID ??= $account.id    
-    } else {
-        Write-Warning "Not logged into Azure CLI, no context to propagate as ARM_TENANT_ID & ARM_SUBSCRIPTION_ID environment variables"
+function Get-OidcRequestUrl()
+{
+    $serviceConnectionId = Get-ServiceConnectionId
+    if (!$serviceConnectionId) {
+        throw "Unable to determine service connection ID"
     }
-} else {
-    Write-Warning "Azure CLI not found, no context to propagate as ARM_TENANT_ID & ARM_SUBSCRIPTION_ID environment variables"
+    $oidcRequestUrl = "${env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}${env:SYSTEM_TEAMPROJECTID}/_apis/distributedtask/hubs/build/plans/${env:SYSTEM_PLANID}/jobs/${env:SYSTEM_JOBID}/oidctoken?api-version=7.1-preview.1&serviceConnectionId=${serviceConnectionId}"
+    Write-Debug "OIDC Request URL: ${oidcRequestUrl}"
+    return $oidcRequestUrl
 }
+
+function Get-ServiceConnectionId()
+{
+    # Get Service Connection ID
+    Get-ChildItem -Path Env: -Recurse -Include ENDPOINT_DATA_* | Sort-Object -Property Name `
+                                                               | Select-Object -First 1 -ExpandProperty Name `
+                                                               | ForEach-Object { $_ -replace 'ENDPOINT_DATA_','' } `
+                                                               | Set-Variable serviceConnectionId
+
+    Write-Verbose "Service Connection ID: ${serviceConnectionId}"
+    return $serviceConnectionId
+}
+
+function New-OidcToken()
+{
+    Write-Verbose "`nRequesting OIDC token from Azure DevOps..."
+    oidcRequestToken = Get-OidcRequestToken
+    oidcRequestUrl   = Get-OidcRequestUrl
+    Invoke-RestMethod -Headers @{
+                        Authorization  = "Bearer ${oidcRequestToken}"
+                        'Content-Type' = 'application/json'
+                      } `
+                      -Uri "${oidcRequestUrl}" `
+                      -Method Post | Set-Variable oidcTokenResponse
+    $oidcToken = $oidcTokenResponse.oidcToken
+    if (!$oidcToken) {
+        throw "Could not get OIDC token"
+    }
+    if ($oidcToken -notmatch "^ey") {
+        throw "OIDC token in unexpected format"
+    }
+    return $oidcToken
+}
+
+if ($env:SYSTEM_DEBUG -eq "true") {
+    $InformationPreference = "Continue"
+    $VerbosePreference = "Continue"
+    $DebugPreference = "Continue"
+}
+
+# Propagate Azure context to Terraform
+az account show 2>$null | ConvertFrom-Json | Set-Variable account
+if (!$account) {
+    throw "Not logged into Azure CLI, no context to propagate as ARM_* environment variables"
+}
+if (![guid]::TryParse($account.user.name, [ref][guid]::Empty)) {
+    throw "Azure CLI logged in with a User Principal instead of a Service Principal"
+}
+$env:ARM_CLIENT_ID       ??= $account.user.name
+$env:ARM_CLIENT_SECRET   ??= $env:servicePrincipalKey # requires addSpnToEnvironment: true
+$env:ARM_TENANT_ID       ??= $account.tenantId
+$env:ARM_SUBSCRIPTION_ID ??= $account.id  
+
+if ($env:ARM_CLIENT_SECRET) {
+    Write-Verbose "Using ARM_CLIENT_SECRET"
+} else {
+    $env:ARM_OIDC_TOKEN  ??= New-OidcToken    
+    Write-Verbose "Using ARM_OIDC_TOKEN"
+}
+Write-Host "Terraform azure provider environment variables:"
+Get-ChildItem -Path Env: -Recurse -Include ARM_* | Select-Object -Property Name `
+                                                 | Sort-Object -Property Name `
+                                                 | Format-Table -HideTableHeader

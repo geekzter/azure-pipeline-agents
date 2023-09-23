58facb1896ba5a3da23694fa56acccf0d55b2a19
Eric van Wijk <eric@van-wijk.com>
2023-09-23 11:31:20 +0200

    Replace VHDs with Managed Images (#96)
    - Let Packer create Managed Images instead of VHDs

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,10 +67,6 @@ locals {
       PIPELINE_DEMO_PACKER_BUILD_RESOURCE_GROUP_NAME            = split("/",module.packer.build_resource_group_id)[4]
       PIPELINE_DEMO_PACKER_LOCATION                             = var.location
       PIPELINE_DEMO_PACKER_POLICY_SET_NAME                      = var.configure_access_control ? module.packer.policy_set_name : ""
-      PIPELINE_DEMO_PACKER_STORAGE_ACCOUNT_ID                   = module.packer.storage_account_id
-      PIPELINE_DEMO_PACKER_STORAGE_ACCOUNT_NAME                 = module.packer.storage_account_name
-      PIPELINE_DEMO_PACKER_STORAGE_ACCOUNT_RESOURCE_GROUP_ID    = join("/",slice(split("/",module.packer.storage_account_id),0,5))
-      PIPELINE_DEMO_PACKER_STORAGE_ACCOUNT_RESOURCE_GROUP_NAME  = split("/",module.packer.storage_account_id)[4]
       PIPELINE_DEMO_PACKER_SUBNET_NAME                          = module.packer.packer_subnet_name
       PIPELINE_DEMO_PACKER_VIRTUAL_NETWORK_ID                   = module.packer.virtual_network_id
       PIPELINE_DEMO_PACKER_VIRTUAL_NETWORK_NAME                 = split("/",module.packer.virtual_network_id)[8]

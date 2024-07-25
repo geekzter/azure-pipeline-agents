b5022cbb2a43938721c2ad77ffe4b1a0b76f16b6
Eric van Wijk <eric@van-wijk.com>
2024-07-25 13:08:12 +0200

    Disable diag storage auth (#119)

--- terraform/modules/azure-packer/network.tf
+++ terraform/modules/azure-packer/network.tf
@@ -61,7 +61,7 @@ resource azurerm_subnet private_endpoint_subnet {
   virtual_network_name         = azurerm_virtual_network.packer.name
   resource_group_name          = azurerm_virtual_network.packer.resource_group_name
   address_prefixes             = [cidrsubnet(azurerm_virtual_network.packer.address_space[0],4,5)]
-  private_endpoint_network_policies_enabled = true
+  private_endpoint_network_policies = "Disabled"
 
   depends_on                   = [
     azurerm_network_security_group.default
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -7,8 +7,8 @@ resource azurerm_storage_account diagnostics {
   account_replication_type     = "LRS"
   allow_nested_items_to_be_public = false
   default_to_oauth_authentication = true
-  enable_https_traffic_only    = true
-  shared_access_key_enabled    = true
+  enable_https_traffic_only    = false
+  shared_access_key_enabled    = false
 
   tags                         = local.tags
 }
@@ -179,7 +179,7 @@ resource azurerm_storage_account share {
   account_replication_type     = "LRS"
   default_to_oauth_authentication = true
   enable_https_traffic_only    = false # Needs to be off for NFS
-  shared_access_key_enabled    = true # Azire Files Share does not support Entra ID AuthN yet
+  shared_access_key_enabled    = true # Azure Files Share does not support Entra ID AuthN yet
 
   tags                         = local.tags
 
